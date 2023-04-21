afd25ae72e5c135acef961add54bc5fa6fb06313
Matt Kiazyk <mattkiazyk@gmail.com>
2023-04-21 13:51:17 -0500

    update homebrew-made to xcodesorg

---
+++ Formula/xcodes.rb
@@ -0,0 +1,20 @@
+class Xcodes < Formula
+  desc "Install and switch between multiple versions of Xcode"
+  homepage "https://www.xcodes.app"
+  url "https://github.com/XcodesOrg/xcodes.git", tag: "1.3.0"
+  head "https://github.com/XcodesOrg/xcodes.git", branch: "main"
+
+  bottle do
+    root_url 'https://github.com/XcodesOrg/xcodes/releases/download/1.3.0'
+    sha256 cellar: :any_skip_relocation, mojave: "f3908cec47dad544712e15133c713fde24f51ccd463aa582e856852be9bdb08e"
+    sha256 cellar: :any_skip_relocation, arm64_mojave: "f3908cec47dad544712e15133c713fde24f51ccd463aa582e856852be9bdb08e"
+  end
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/xcodes"
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# XcodesOrg Homebrew Formulae
+
+- [xcodes](https://github.com/xcodesOrg/xcodes): Manage the Xcodes installed on your Mac
+
+
+## Maintainers
+
+[Matt Kiazyk](https://github.com/mattkiazyk) - [Twitter](https://www.twitter.com/mattkiazyk)
