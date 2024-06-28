faf434fbbe869a5744b74ee3476c048aa45fb602
Matt Kiazyk <mattkiazyk@gmail.com>
2024-06-28 10:02:46 -0500

    Update xcodes.rb 1.5.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
   desc "Install and switch between multiple versions of Xcode"
   homepage "https://www.xcodes.app"
-  url "https://github.com/XcodesOrg/xcodes.git", tag: "1.4.1"
+  url "https://github.com/XcodesOrg/xcodes.git", tag: "1.5.0"
   head "https://github.com/XcodesOrg/xcodes.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/XcodesOrg/xcodes/releases/download/1.4.1'
-    sha256 cellar: :any_skip_relocation, mojave: "f9c66503f1a2d5d9d7f48ea053a2739b35dd0f7325e668ff4c1062539416b409"
-    sha256 cellar: :any_skip_relocation, arm64_mojave: "f9c66503f1a2d5d9d7f48ea053a2739b35dd0f7325e668ff4c1062539416b409"
+    root_url 'https://github.com/XcodesOrg/xcodes/releases/download/1.5.0'
+    sha256 cellar: :any_skip_relocation, mojave: "348f58b7ba7e7a0ad4c63cfd3609560a228cbe371def20740b1e44c8c41684cd"
+    sha256 cellar: :any_skip_relocation, arm64_mojave: "348f58b7ba7e7a0ad4c63cfd3609560a228cbe371def20740b1e44c8c41684cd"
   end
 
   def install
