5aa0dc3692494f3473666bb68aa50cb03804edb6
Matt Kiazyk <mattkiazyk@gmail.com>
2025-06-10 21:35:24 -0500

    Update xcodes.rb

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
   desc "Install and switch between multiple versions of Xcode"
   homepage "https://www.xcodes.app"
-  url "https://github.com/XcodesOrg/xcodes.git", tag: "1.6.1"
+  url "https://github.com/XcodesOrg/xcodes.git", tag: "1.6.2"
   head "https://github.com/XcodesOrg/xcodes.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/XcodesOrg/xcodes/releases/download/1.6.1'
-    sha256 cellar: :any_skip_relocation, mojave: "9f206a012c5d0a45abdd45ec28476c8d503622276bb276b6d23f2aacf3686643"
-    sha256 cellar: :any_skip_relocation, arm64_mojave: "9f206a012c5d0a45abdd45ec28476c8d503622276bb276b6d23f2aacf3686643"
+    root_url 'https://github.com/XcodesOrg/xcodes/releases/download/1.6.2'
+    sha256 cellar: :any_skip_relocation, mojave: "c4530102af12850d63fd674bdcf6030649be47e1f7d283450b6f526c9285c4f7"
+    sha256 cellar: :any_skip_relocation, arm64_mojave: "c4530102af12850d63fd674bdcf6030649be47e1f7d283450b6f526c9285c4f7"
   end
 
   def install
